b8c2fb1840f2409903795be1ba99c3818a48f463
pcentric <priyanshu.s1619@gmail.com>
2022-01-30 18:54:44 +0530

    changed in next js config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
-    domains: ["links.papareact.com", "image.tmdb.org"],
+    domains: ["links.papareact.com", "image.tmdb.org", "assets.vercel.com"],
+    formats: ["image/avif", "image/webp", "image/jpg", "image/png"],
   },
 };
